19e1329e3f4c6ac122b4ec246dd6e0bb2c5a46ad
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2018-02-06 11:20:43 +0700

    clean up

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -12,15 +12,15 @@ import { trigger, style, animate, transition, keyframes, query, stagger } from "
         query(':enter', style({ opacity : 0 }), { optional : true }),
         query(':enter', stagger('300ms', [
           animate('.6s ease-in', keyframes([
-            style({ opacity: 0, transform: 'translateY(-75%)', offset: 0}),
-            style({ opacity: .5, transform: 'translateY(35px)', offset: .3}),
-            style({ opacity: 1, transform: 'translateY(0)', offset: 1}),
+            style({ opacity: 0, transform: 'translateX(0)', offset: 0}),
+            style({ opacity: .5, transform: 'translateX(35px)', offset: .5}),
+            style({ opacity: 1, transform: 'translateX(0)', offset: 1}),
           ]))]), { optional : true }),
         query(':leave', stagger('300ms', [
           animate('.6s ease-in', keyframes([
-            style({ opacity: 1, transform: 'translateY(0)', offset: 0}),
-            style({ opacity: .5, transform: 'translateY(35px)', offset: .3}),
-            style({ opacity: 0, transform: 'translateY(-75%)', offset: 1}),
+            style({ opacity: 1, transform: 'translateX(0)', offset: 0}),
+            style({ opacity: .5, transform: 'translateX(35px)', offset: .5}),
+            style({ opacity: 0, transform: 'translateX(0)', offset: 1}),
           ]))]), { optional : true }),
       ])
     ])
@@ -67,7 +67,7 @@ export class DashboardComponent implements OnInit {
   closeModal(){
     this.notification = false;
   }
-  
+
   removeItem(index){
     this.queuedBills.details.splice(index, 1);
   }
