166e3ed1260a2bb13c89e796163452ec7be81330
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2018-02-10 19:04:15 +0700

    implement login flow

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router }      from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  message: string;
 
-  constructor() { }
+  constructor(public authService: AuthService, public router: Router) {
+    this.setMessage();
+   }
 
   ngOnInit() {
+  }
+
+  setMessage(){
+    this.message = 'Logged ' + (this.authService.isLoggedIn ? 'in':'out');
+  }
+
+  login(){
+    this.message = 'Trying to log in ...';
+
+    this.authService.login().subscribe(
+      () => {
+        this.setMessage();
+        if (this.authService.isLoggedIn) {
+          let redirect = this.authService.redirectUrl ? this.authService.redirectUrl : '/login';
+          this.router.navigate([redirect]);
+          
+        }
+      }
+    );
+  }
+
+  logout(){
+    this.authService.logout();
+    this.setMessage();
   }
 
 }
