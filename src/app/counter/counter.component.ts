bf1fe4ddada4a8c094fa6ca901a6922872e04c1d
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2018-03-01 06:57:29 +0700

    Table untuk detail tagihan

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -2,11 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import { String } from 'typescript-string-operations';
 import { BillsService } from "../bills.service";
 import { Bills } from "../bills.enum";
+import { trigger, style, animate, transition, keyframes, query, stagger } from "@angular/animations";
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
-  styleUrls: ['./counter.component.css']
+  styleUrls: ['./counter.component.css'],
+  animations: [
+    trigger('bills',[
+      transition('* => *', [
+        query(':enter', style({ opacity : 0 }), { optional : true }),
+        query(':enter', stagger('100ms', [
+          animate('.6s ease-in', keyframes([
+            style({ opacity: 0, transform: 'translateX(-20%)', offset: 0}),
+            style({ opacity: .5, transform: 'translateX(10px)', offset: .3}),
+            style({ opacity: 1, transform: 'translateX(0)', offset: 1}),
+          ]))]), { optional : true }),
+        query(':leave', stagger('0ms', [
+          animate('.6s ease-in', keyframes([
+            style({ opacity: 1, transform: 'translateX(0)', offset: 0}),
+            style({ opacity: 0, transform: 'translateX(-20%)', offset: 1}),
+          ]))]), { optional : true }),
+      ])
+    ])
+  ]
 })
 export class CounterComponent implements OnInit {
   message: String;
@@ -20,6 +39,7 @@ export class CounterComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.queuedBills = {};
     this.message = null;
     this.error = {};
   }
@@ -59,6 +79,44 @@ export class CounterComponent implements OnInit {
       
     }
     
+  }
+
+  showModal(stats: boolean){
+    let idx = [];
+    for(let i = 0; i < this.queuedBills.details.length; i++){
+      idx.push(this.queuedBills.details[i].ID_TAGIHAN);
+    }
+    console.log(stats);
+    if(stats){
+      console.log("Queued bills dilunasi");
+      this.payQueuedBills(idx, Bills.PAID);
+    }else{
+      console.log("Queued bills dibatalkan");
+      this.payQueuedBills(idx, Bills.IDLE);
+    }
+  }
+
+  payQueuedBills(idx, stats){
+    let obj = {
+      "id": idx,
+      "status": stats
+    }
+
+    this._bills.postBills(obj).subscribe(
+      result => {
+        console.log(result);
+        if((result.affectedRows > 0) && (result.warningCount == 0))
+        {
+          this.queuedBills = {};
+        }
+
+        if(stats == Bills.PAID){
+          this.message = "Pembayaran tagihan berhasil";
+        }else{
+          this.message = "Pembayaran tagihan dibatalkan";
+        }
+      }
+    );
   }
 
 }
